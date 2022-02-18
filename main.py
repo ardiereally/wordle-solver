e6d7b1175523bcce37e6fae2afe0e5812c94861b
rdas <rounakdas818@gmail.com>
2022-02-18 15:31:20 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import random
+
+
+def load_words():
+    with open('/usr/share/dict/words') as d:
+        return [line.rstrip().lower() for line in d.readlines()]
+
+
+def right_size():
+    def f(word):
+        return len(word) == 5
+
+    return f
+
+
+def correct_spot(char, pos):
+    def f(word):
+        return word[pos] == char
+
+    return f
+
+
+def wrong_spot(char, pos):
+    def f(word):
+        return char in word and word[pos] != char
+
+    return f
+
+
+def doesnt_contain(char):
+    def f(word):
+        return char not in word
+
+    return f
+
+
+if __name__ == '__main__':
+    words = load_words()
+    constraint_list = [
+        right_size(),
+        # add constraints after putting the guess
+    ]
+
+    for constraint in constraint_list:
+        words = list(filter(constraint, words))
+
+    print(f"Left with {len(words)} words")
+    guess = random.choice(words)
+
+    print(f"Guess: {guess}")
